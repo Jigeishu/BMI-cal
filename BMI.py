0f0a1b810ee7f7f715e9a6e788a7fa4d3a9a5f94
Jigeishu Srivastava <113457202+Jigeishu@users.noreply.github.com>
2022-10-08 10:20:56 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,12 @@
+weight= float(input( "Enter Weight"))
+height= float(input("Enter Height"))
+BMI = weight / (height**2)
+
+if BMI < 18.5:
+    print("Underweight")
+elif 18.5 <= BMI <25:
+    print("Normal")
+elif 25<= BMI <30:
+    print("Overweight")
+elif BMI > 30:
+    print("Obesity")
